83ea23f4708fa607ba3b34bb1ab0be5d302f5b32
Your Name <you@example.com>
2018-04-20 16:30:16 +0200

    return value fittizio

--- src/marketPlace/Collection.java
+++ src/marketPlace/Collection.java
@@ -10,10 +10,13 @@ public class Collection {
 
     protected boolean addCardToColl(Card card) {
         // to be defined
+        return false; //solo di prova
     }
 
     protected Card[] searchByString(String string) {
         // to be defined
+        Card[] cards = new Card[0];    //solo di prova
+        return cards;
     }
 
     protected void remCardFromColl(Card card) {
