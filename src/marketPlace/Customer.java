f05c77db97782c9b4b5dddf91f4f52628b4b9f17
Marco Arazzi <marco@localhost.localdomain>
2018-04-20 16:55:14 +0200

    Modifica Customer

--- src/marketPlace/Customer.java
+++ src/marketPlace/Customer.java
@@ -1,4 +1,43 @@
 package marketPlace;
 
 public class Customer {
+
+    private String user;
+    private String pssw;
+    private Collection collection;
+
+    protected boolean addCard(Card card){
+        if(collection.addCardToColl(card)){
+            return true;
+        }
+
+        return false;
+    }
+
+    protected boolean rmCard(Card card){
+        if(collection.remCardFromColl(card)){
+            return  true;
+        }
+
+        return  false;
+    }
+
+    protected boolean createCard(Description d, int id){
+
+        Card newCard = new Card(id, d);
+
+        if(addCard(newCard)){
+
+            return true;
+
+        }
+
+        return  false;
+    }
+
+    /*protected Card[] searchByString(String s){
+
+
+
+    }*/
 }
