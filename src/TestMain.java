9665efb241b5c65ffaa48c6f7da4ddb6765fa4f4
Your Name <you@example.com>
2018-04-20 17:00:39 +0200

    test per Description. Cambiare URL dell'immagine per provarlo

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,2 +1,18 @@
+import marketPlace.Description;
+
+import java.io.IOException;
+
 public class TestMain {
+    public static void main(String[] args) {
+
+        //----------------Prova Description---------------------
+        String urlPic="./dragoOcchiBlu.jpg";
+        try {
+            Description descr1=new Description("Drago Bianco Occhi Blu","8 stelle di potenza",urlPic);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //------------------------------------------------------
+    }
+
 }
