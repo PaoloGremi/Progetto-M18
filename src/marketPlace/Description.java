ac0f9015b9326e251b434860354dd1ca1f290dd1
Your Name <you@example.com>
2018-04-20 16:07:18 +0200

    messo a posto @param

--- src/marketPlace/Description.java
+++ src/marketPlace/Description.java
@@ -1,6 +1,9 @@
 package marketPlace;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 
 public class Description {
@@ -8,22 +11,52 @@ public class Description {
     /**
      * @param name: Card's name
      * @param pic: Card's picture to be displayed on GUI
-     * @param tag: Card's list of tags to be used during search/filter
-     * @param description: Card's text description
+     * @param picUrl: Url to load picture
+     * @param listTag: Card's list of tags to be used during search/filter
+     * @param text: Card's text description
      * ? @param cardType: Type of card
       */
-    
+    private String name;
+    private String text; //cambiare UML
+    private String picUrl;
+    private BufferedImage pic; //formati supportati: GIF,PNG,JPEG,BMP,WBMP
+    private HashSet<String> listTag;
+
+
+    public Description(String name,String text,String picUrl) {
+        this.name=name;
+        this.text=text;
+        this.picUrl=picUrl;
+        this.pic=null;
+    }
+
+    private void loadImage(String url) throws IOException {
+        File picFile= new File(url);
+        pic= ImageIO.read(picFile);
+    }
+
+    public boolean addTag(String tag ){
+        String tagNoSens=tag.toLowerCase(); // Per non rip
+        return listTag.add(tagNoSens);
+
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public BufferedImage getPic() {
+        return pic;
+    }
 
 
     public String getName() {
         return name;
     }
 
-    public HashSet<String> getTag() {
-        return tag;
+    public HashSet<String> getListTag() {
+        return listTag;
     }
 
-    public String getDescription() {
-        return description;
-    }
+
 }
