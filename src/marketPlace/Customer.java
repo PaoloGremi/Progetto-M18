ad1b2f219e3960ec09899f157c13af061957b05a
crenar8 <32876067+crenar8@users.noreply.github.com>
2018-04-23 12:03:41 +0200

    Update Customer.java

--- src/marketPlace/Customer.java
+++ src/marketPlace/Customer.java
@@ -6,41 +6,35 @@ public class Customer {
     private String pssw;
     private Collection collection;
 
+    // Add a card to the card collection of the customer who calls this method.
     protected boolean addCard(Card card){
         if(collection.addCardToColl(card)){
             return true;
         }
-
         return false;
     }
 
+    // Remove a card to the card collection of the customer who calls this method.
     protected boolean rmCard(Card card){
         if(collection.remCardFromColl(card)){
             return  true;
         }
-
         return  false;
     }
 
+    // The customer adds a new card that wasn't in the card catalog before.
     protected boolean createCard(Description d, int id){
-
         Card newCard = new Card(id, d);
-
         if(addCard(newCard)){
-
             return true;
-
         }
-
         return  false;
     }
-   //prova
+   
+    // Search a card by its identificator.
     protected Card[] searchByString(String s){
-
         Card[] cardsFound = collection.searchByString(s);
-
         return cardsFound;
-
     }
 
 }
