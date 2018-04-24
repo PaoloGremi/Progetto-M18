9389ac3e71b95afb5cb9d9679346f0c433d24fa5
TeoGore <matteo.goretti01@universitadipavia.it>
2018-04-25 00:31:46 +0200

    aggiunti javadocs

--- src/TradeCenter/Card/CardType.java
+++ src/TradeCenter/Card/CardType.java
@@ -1,5 +1,10 @@
 package TradeCenter.Card;
 
+/**
+ * Card types used in the trading exchanges.
+ *
+ * It can be, for now, only Yugioh.
+ */
 public enum CardType {
 
     YuGiOh;
--- src/TradeCenter/TradeCenter.java
+++ src/TradeCenter/TradeCenter.java
@@ -9,6 +9,12 @@ import TradeCenter.Customers.Customer;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Class representing the trading center.
+ *
+ * It keeps a list of customers that interacts with each other
+ * Customers have their own collection, taken from a database
+ */
 public class TradeCenter {
 
     private int contUsers;                              //todo: CAMBIARE QUANDO MIGLIORO ID UTENTE
@@ -17,6 +23,9 @@ public class TradeCenter {
     private ArrayList<Trade> activeTrades;
     private ArrayList<Trade> doneTrades;
 
+    /**
+     * Create a new trade center, with  the database and no users
+     */
     public TradeCenter() {
         this.contUsers = 0;                             //todo: CAMBIARE QUANDO MIGLIORO ID UTENTE
         this.catalog = new CardCatalog();                  //todo: fare mettere nell'istanziazione del catalogo la creazione del database
@@ -25,21 +34,43 @@ public class TradeCenter {
         this.doneTrades = new ArrayList<Trade>();
     }
 
+    /**
+     * Create an account for a new user
+     *
+     * @param username
+     * @param password
+     */
     public void addCustomer(String username, String password) {
         String id = customerID();
         customers.put(id, new Customer(id,username,password));
     }
 
+    /**
+     * Create a unique ID for the user
+     *
+     * @return the ID
+     */
     private String customerID(){
         contUsers++;                                    //todo: CAMBIARE QUANDO MIGLIORO ID UTENTE
         return "USER-" + contUsers;
     }
 
+    /**
+     * Allow the user to delete his account
+     *
+     * @param id user identifier
+     */
     public void removeCustomer(int id) {
         customers.remove(id);                           //capire come passare id
     }
 
-    public Customer searchCustomer(String username){
+    /**
+     * User can find another user by searching his name
+     *
+     * @param username
+     * @return
+     */
+    public Customer searchCustomer(String username){        //todo: potrebbe ritornare solo l'ID--> do meno info a chi non le deve avere
         for(String key : customers.keySet()){
             if((customers.get(key)).getUsername().equals(username)){
                 return customers.get(key);
@@ -48,12 +79,24 @@ public class TradeCenter {
         return null;
     }
 
+    /**
+     * If a card doesn't exist in the database, the user can add it
+     *
+     * @param description the card
+     */
     public void addDescription(Description description){
         catalog.addDescription(description);            //todo secondo me il catalog non va messo qua come attributo(diventano 2 istanze diverse), RIVEDERE
     }
 
     //todo: puo essere utile mettere il metodo remove description ??
-    public HashMap<Customer, Card[]> searchByString(String searchString){
+
+    /**
+     * Users can search a card, they see all users that match the search
+     *
+     * @param searchString name or description of a card
+     * @return a list of customers with their own collections
+     */
+    public HashMap<Customer, Card[]> searchByString(String searchString){       //todo ritorna card[] o meglio collection??
         HashMap<Customer, Card[]> tmp = new HashMap<>();
         for(String key : customers.keySet()){
             tmp.put(customers.get(key), customers.get(key).searchByString(searchString));
@@ -61,12 +104,20 @@ public class TradeCenter {
         return tmp;
     }
 
+    /**
+     * Do the exchange, users collection are update, and so the trade
+     *
+     * @param trade a card exchange
+     */
     void switchCards(Trade trade){
             //ricontrollare trade prima di fare questo metodo e il successivo
         //devo aspettare che venga definito lo scambio delle carte
     }
 
-
+    /**
+     * Check for deals that are over, if so it updates the list of active and done deals
+     * Also if a deal it's over, the exchange is done
+     */
     public void checkDoneDeals(){
         for(Trade trade : activeTrades){
             if(trade.isDoneDeal()){
