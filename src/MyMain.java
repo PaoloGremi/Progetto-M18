609efb62d36af8735e489a5cffe65e1a695cfafd
Matteo Goretti <32876096+TeoGore@users.noreply.github.com>
2018-04-25 16:04:47 +0200

    aggiunto per sbaglio in un fix
    nel fix dei package ho fatto add si src, dimenticandomi ci fosse dentro il mio tester locale

--- src/MyMain.java
+++
@@ -1,9 +0,0 @@
-import TradeCenter.TradeCenter;
-
-public class MyMain {
-    public static void main(String[] args) {
-        TradeCenter tradeCenter = new TradeCenter();
-
-        //utilizzare per fare le mie prove
-    }
-}
