78c18a15c8e0f86572eb0f7073a80fdc76bc30bd
Andre96 <andreacatena96@icloud.com>
2018-05-15 14:53:55 +0200

    log in and sign up interface

--- src/Interface/SignUp.java
+++ src/Interface/SignUp.java
@@ -4,6 +4,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -11,26 +12,33 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import TradeCenter.Customers.Customer;
 
-public class LogIn {
+public class SignUp {
     static boolean correctcredentials;
     public static boolean display(){
 
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle("LogIn window");
+        window.setTitle("SignUp window");
         VBox logInLayout = new VBox(20);
-        Label label1 = new Label("insert username and password");
+        Label label1 = new Label("insert username and password, and repeat the password");
         TextField username = new TextField("Username");
-        TextField password = new TextField("Password");
+        username.setPromptText("Username");
+       //create password field
+        PasswordField password = new PasswordField();
+        password.setPromptText("Password");
+       //create the check for the password
+        PasswordField passwordVerifier = new PasswordField();
+        passwordVerifier.setPromptText("Repeat the password");
+
         Button submit = new Button("Submit");
         // è solo una prova poi implementeremo il metodo di controllo, per adesso è sempre vero che mette la giusta password e id
         submit.setOnAction(event -> {
             correctcredentials = true;
             window.close();
         });
-        logInLayout.getChildren().addAll(label1, username, password, submit);
+        logInLayout.getChildren().addAll(label1, username, password, passwordVerifier, submit);
         logInLayout.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(logInLayout, 300, 200);
+        Scene scene = new Scene(logInLayout, 500, 200);
         window.setScene(scene);
         window.showAndWait();
         return correctcredentials;
