9ce42f4356891fa7c332da0b4ef21aa55e5a0e1b
Andre96 <andreacatena96@icloud.com>
2018-05-15 14:52:04 +0200

    log in and sign up interface

---
+++ src/LogIn.java
@@ -0,0 +1,54 @@
+import Interface.SignUp;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class LogIn extends Application{
+
+    Stage window;
+    Button logIn, signUp;
+    TextField username;
+    PasswordField password;
+    Label credentials;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("LogIn interface");
+        signUp = new Button("SignUp");
+        signUp.setOnAction(event -> {
+            boolean Logged = SignUp.display();
+        });
+        logIn = new Button("LogIn");
+        //fare l'azione per login
+        credentials = new Label("Please enter username and password");
+        username = new TextField("Username");
+        password = new PasswordField();
+        password.setPromptText("Password");
+        VBox credentialBox = new VBox();
+        credentialBox.getChildren().addAll(credentials, username, password);
+        credentialBox.setSpacing(20);
+
+        HBox logIn_Register = new HBox();
+        logIn_Register.getChildren().addAll(logIn, signUp);
+        logIn_Register.setSpacing(30);
+        logIn_Register.setAlignment(Pos.CENTER);
+
+        BorderPane layout = new BorderPane();
+        layout.setCenter(credentialBox);
+        layout.setBottom(logIn_Register);
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+    }
+}
