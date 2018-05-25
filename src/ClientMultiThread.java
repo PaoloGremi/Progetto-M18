c69a4353c21e36377011207804722425c75ef27b
MarcoArazzi <marcoarazzi96@gmail.com>
2018-05-25 14:45:56 +0200

    MultiThreadClient

---
+++ src/ClientMultiThread.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+
+class Client extends Thread{
+
+            @Override
+            public void run() {
+                Application.launch(LogIn.class);
+            }
+
+}
+
+public class ClientMultiThread{
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Client();
+        t1.start();
+        t1.join();
+    }
+
+}
