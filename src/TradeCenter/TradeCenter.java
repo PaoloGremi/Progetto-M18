b495edf3c3625a76d979d992fa4cc7f80cdaeafe
TeoGore <matteo.goretti01@universitadipavia.it>
2018-06-05 17:55:40 +0200

    tradecenter refactor

--- src/TradeCenter/TradeCenter.java
+++ src/TradeCenter/TradeCenter.java
@@ -27,7 +27,7 @@ import java.util.HashSet;
  */
 public class TradeCenter {
 
-    private int contUsers;                              //todo: Migliorare ID utente
+    private int contUsers;
     private CardCatalog pokemonCatalog;
     private CardCatalog yugiohCatalog;
     private HashMap<String, Customer> customers;
@@ -36,7 +36,7 @@ public class TradeCenter {
     private DBProxy proxy;
 
     /**
-     * Create a new trade center, with  the database and no users
+     * Create a new trade center, with the database connection that load cards and users with their attributes
      */
     public TradeCenter() {
         this.contUsers = 0;
@@ -50,7 +50,6 @@ public class TradeCenter {
         this.activeTrades = new ArrayList<Trade>();
         this.doneTrades = new ArrayList<Trade>();
     }
-
     /**
      * Create an account for a new user
      */
@@ -64,6 +63,7 @@ public class TradeCenter {
             proxy.insertCustomer(temporaryCustomer);
         }
     }
+
 
     /**
      * A method that check if the username is already in use
@@ -89,132 +89,6 @@ public class TradeCenter {
     private String customerID(){
         contUsers++;
         return "USER-" + contUsers;
-    }
-
-    /**
-     * User can find another user by searching his name
-     * @return
-     */
-    public Customer searchCustomer(String username){        //todo: potrebbe ritornare solo l'ID--> do meno info a chi non le deve avere
-        for(String key : customers.keySet()){
-            if((customers.get(key)).getUsername().equals(username)){
-                return customers.get(key);
-            }
-        }
-        //user not found
-        throw new UserNotFoundException();
-    }
-
-    //todo add javadocs
-    public ArrayList<Customer> searchUsers(String username, String myUsername){
-        ArrayList<Customer> results = new ArrayList<>();
-        String usernameLow = username.toLowerCase();
-        for(String key: customers.keySet()){
-            String name = customers.get(key).getUsername().toLowerCase();
-            if(name.contains(usernameLow) && !customers.get(key).getUsername().equals(myUsername)){
-                results.add(customers.get(key));
-            }
-        }
-        return results;
-    }
-
-    //todo add javadocs
-    public ArrayList<HashMap<Customer, Collection>> searchByDescription(Description description){
-        ArrayList<HashMap<Customer, Collection>> cards = new ArrayList<HashMap<Customer, Collection>>();
-
-        for(String key: customers.keySet()){
-            HashMap<Customer, Collection> customerCollection = customers.get(key).searchByDescription(description);
-                if(customerCollection!=null) {
-                    cards.add(customerCollection);
-                }
-
-        }
-
-        return cards;
-    }
-
-    //todo add javadocs
-    public void removeFromWishList(Description cardDescription, Customer customer) {
-
-        customer.removeFromWishList(cardDescription);
-
-    }
-
-    //todo add javadocs
-    //private boolean possibleSearch(String search){
-      ///  return true;        //todo mettere ricerca vera , per ora fa vedere il nome di tutti  i customer
-    //}
-
-    /**
-     * Method that search if the card is in the database or not
-     *
-     * @param description the card that we need to check
-     * @return boolean True if the card is in the database
-     */
-    public boolean searchCard(Description description){
-        //fino a che il metodo non c'è deve ritornare sempre true
-        return true;                //todo mettere il metodo che cerca nel DB, PARLARE CON FEDE
-    }
-
-    /**
-     * Users can search a card, they see all users that match the search
-     *
-     * @param searchString name or description of a card
-     * @return a list of customers with their own collections
-     */
-    public HashMap<Customer, Collection> searchByString(String searchString){
-        HashMap<Customer, Collection> searched = new HashMap<>();
-        for(String key : customers.keySet()){
-            searched.put(customers.get(key), customers.get(key).searchByString(searchString));
-        }
-        if(searched.size() == 0){
-            //nothing found
-            throw new CardNotFoundException();          //todo eccezione da risolvere, se nessuno ha la carta il software deve continuare
-        }                                               //todo PARLARE CON FEDE
-        return searched;
-    }
-
-    /**
-     * This method starts a new trade between two customers
-     * @param offer the first offer
-     */
-    void createTrade(Offer offer){
-        Trade Trade = new Trade(offer);      //todo mi aspetto che l'offerta si crei dall'interfaccia grafica, o desicedere di istanziarla qua
-        activeTrades.add(Trade);
-    }
-    /**
-     * Do the exchange, users collection are update, and so the trade
-     *
-     * @param trade a card exchange
-     */
-    private void switchCards(ATrade trade){
-        if(activeTrades.contains(trade)) {
-
-            Customer customer1 = trade.getCustomer1();
-            Customer customer2 = trade.getCustomer2();
-
-            for (Card card : trade.getOffer1()) {       //take card offered from customer1, add to customer2
-                customer2.addCard(card);
-                customer1.removeCard(card);
-            }
-            for (Card card : trade.getOffer2()) {       //take card offered from customer2, add to customer1
-                customer1.addCard(card);
-                customer2.removeCard(card);
-            }
-            proxy.updateCustomer(customer1);
-            proxy.updateCustomer(customer2);
-
-        }else{
-            throw new NoSuchTradeException();
-        }
-    }
-
-    /**
-     * A Method that updates the customer's attributes, like wishlist
-     * @param customer the user that has to be updated
-     */
-    private void updateCustomer(Customer customer){
-        proxy.updateCustomer(customer);
     }
 
     /**
@@ -255,17 +129,73 @@ public class TradeCenter {
     }
 
     /**
-     * Check for deals that are over, if so it updates the list of active and done deals
-     * Also if a deal it's over, the exchange is done
+     * A method that removes a card from a Customer Wishlist
+     * @param cardDescription the card to remove
+     * @param customer the customer that wants to remove the card
      */
-    public void checkDoneDeals(){
-        for(Trade trade : activeTrades){        //todo togliere ciclo, ricontrollare per sincronia con interfaccia grafica e db
-            if(trade.isDoneDeal()){
-                switchCards(trade);
-                doneTrades.add(trade);
-                activeTrades.remove(trade);
+    public void removeFromWishList(Description cardDescription, Customer customer) {
+
+        customer.removeFromWishList(cardDescription);
+        //todo fare update con proxy
+    }
+
+    /**
+     * A Method that updates the customer's attributes, like wishlist
+     * @param customer the user that has to be updated
+     */
+    private void updateCustomer(Customer customer){
+        proxy.updateCustomer(customer);
+    }
+
+    /**
+     * User can find another user by searching his name
+     * @return
+     */
+    public Customer searchCustomer(String username){
+        for(String key : customers.keySet()){
+            if((customers.get(key)).getUsername().equals(username)){
+                return customers.get(key);
             }
         }
+        //user not found
+        throw new UserNotFoundException();
+    }
+
+    /**
+     * A method that search all the customers that have a username similar to a string searched
+     * @param username the string for the research
+     * @param myUsername a string to exclude myself from the research
+     * @return the list of customers that corresponds to the search
+     */
+    public ArrayList<Customer> searchUsers(String username, String myUsername){
+        ArrayList<Customer> results = new ArrayList<>();
+        String usernameLow = username.toLowerCase();
+        for(String key: customers.keySet()){
+            String name = customers.get(key).getUsername().toLowerCase();
+            if(name.contains(usernameLow) && !customers.get(key).getUsername().equals(myUsername)){
+                results.add(customers.get(key));
+            }
+        }
+        return results;
+    }
+
+    /**
+     *  A method that checks if any of the user has a given card
+     * @param description the card that has to be searched
+     * @return the list of the users that have that card in their collection
+     */
+    public ArrayList<HashMap<Customer, Collection>> searchByDescription(Description description){
+        ArrayList<HashMap<Customer, Collection>> cards = new ArrayList<HashMap<Customer, Collection>>();
+
+        for(String key: customers.keySet()){
+            HashMap<Customer, Collection> customerCollection = customers.get(key).searchByDescription(description);
+                if(customerCollection!=null) {
+                    cards.add(customerCollection);
+                }
+
+        }
+
+        return cards;
     }
 
     /**
@@ -273,7 +203,6 @@ public class TradeCenter {
      * @param pokemonAll
      * @return
      */
-
     public HashSet<PokemonDescription> searchDescrInPokemonDb(PokemonAll pokemonAll) throws NoSuchDescriptionFoundedException {
 
         String type=pokemonAll.getType();
@@ -297,6 +226,74 @@ public class TradeCenter {
     public HashSet<Description> searchDescrInYugiohDb(){
         HashSet<Description> descrMatched=new HashSet<>();
         return descrMatched;
+    }
+
+    /**
+     * Users can search a card, they see all users that match the search
+     *
+     * @param searchString name or description of a card
+     * @return a list of customers with their own collections
+     */
+    public HashMap<Customer, Collection> searchByString(String searchString){
+        HashMap<Customer, Collection> searched = new HashMap<>();
+        for(String key : customers.keySet()){
+            searched.put(customers.get(key), customers.get(key).searchByString(searchString));
+        }
+        if(searched.size() == 0){
+            //nothing found
+            throw new CardNotFoundException();          //todo eccezione da risolvere, se nessuno ha la carta il software deve continuare
+        }                                               //todo PARLARE CON FEDE
+        return searched;
+    }
+
+    /**
+     * This method starts a new trade between two customers
+     * @param offer the first offer
+     */
+    void createTrade(Offer offer){
+        Trade Trade = new Trade(offer);      //todo mi aspetto che l'offerta si crei dall'interfaccia grafica, o desicedere di istanziarla qua
+        activeTrades.add(Trade);
+    }
+
+    /**
+     * Do the exchange, users collection are update, and so the trade
+     *
+     * @param trade a card exchange
+     */
+    private void switchCards(ATrade trade){
+        if(activeTrades.contains(trade)) {
+
+            Customer customer1 = trade.getCustomer1();
+            Customer customer2 = trade.getCustomer2();
+
+            for (Card card : trade.getOffer1()) {       //take card offered from customer1, add to customer2
+                customer2.addCard(card);
+                customer1.removeCard(card);
+            }
+            for (Card card : trade.getOffer2()) {       //take card offered from customer2, add to customer1
+                customer1.addCard(card);
+                customer2.removeCard(card);
+            }
+            proxy.updateCustomer(customer1);
+            proxy.updateCustomer(customer2);
+
+        }else{
+            throw new NoSuchTradeException();
+        }
+    }
+
+    /**
+     * Check for deals that are over, if so it updates the list of active and done deals
+     * Also if a deal it's over, the exchange is done
+     */
+    public void checkDoneDeals(){
+        for(Trade trade : activeTrades){        //todo togliere ciclo, ricontrollare per sincronia con interfaccia grafica e db
+            if(trade.isDoneDeal()){
+                switchCards(trade);
+                doneTrades.add(trade);
+                activeTrades.remove(trade);
+            }
+        }
     }
 
     /**
