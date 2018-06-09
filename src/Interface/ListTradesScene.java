9cb488b333d6c3c8bc41943b18f5b6f94bf581e8
TeoGore <matteo.goretti01@universitadipavia.it>
2018-06-09 23:01:29 +0200

    continue trade list scene

--- src/Interface/ListTradesScene.java
+++ src/Interface/ListTradesScene.java
@@ -3,10 +3,14 @@ package Interface;
 import TradeCenter.Customers.Customer;
 import TradeCenter.TradeCenter;
 import TradeCenter.Trades.Trade;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 
@@ -31,22 +35,38 @@ public class ListTradesScene {
         scrollableList = new ScrollPane();
 
 
-        /*TradeCenter tradeCenter = new TradeCenter();
+        TradeCenter tradeCenter = new TradeCenter();
         tradeCenter.fakeTrades(myCustomer);
         ArrayList<Trade> userActiveTrades = tradeCenter.showUserActiveTrades(myCustomer);   //todo porcata togliere i TradeCenter abusivi
         ArrayList<Trade> userDoneTrades = tradeCenter.showUserDoneTrades(myCustomer);
         ArrayList<Trade> userTrades = new ArrayList<>();
         userTrades.addAll(userActiveTrades);
-        userTrades.addAll(userDoneTrades);*/
+        userTrades.addAll(userDoneTrades);
 
 
-
-        //todo allargare le label che si creano
         ObservableList<Trade> trades = FXCollections.observableArrayList();
-        //trades.addAll(userTrades);
+        trades.addAll(userTrades);
 
         //todo mettere listener che quando schiaccio un trade mi fa vedere o il trade (se attivo), o altro se finito--> fare relativa scena
         ListView<Trade> tradeList = new ListView<>(trades);
+        EventHandler<MouseEvent> eventHandlerBox =
+                new EventHandler<javafx.scene.input.MouseEvent>() {
+                    @Override
+                    public void handle(javafx.scene.input.MouseEvent e) {
+                        MainWindow.refreshDynamicContent(TradeScene.refresh());
+                    }
+                };
+
+        //todo se funziona listener modificare tradeScene (magari metodo refresh)in modo che si apra come l'ultima offerta fatta e non come fosse la prima
+        tradeList.setFixedCellSize(80.0);
+        tradeList.setStyle("-fx-control-inner-background: #feff2e;");
+        //todo trovare come fare colore diverso dei bordi della label, o fare spaziatura in modo da colorare il fondo con setBackground
+        //todo vedi: https://stackoverflow.com/questions/29602718/insert-space-between-cells-in-list-view-javafx?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+        tradeList.setOnMouseClicked(eventHandlerBox);
+        //todo la nullpointer exception probabilmente è perche non esiste il secondo customer, vedere con marco come fixare
+        //TODO infatti se prima si apre il trade con l'utente l'eccezione non si genera, ma Altro PROBLEMA
+        //todo fixare che qualunque item schiaccio fa vedere la trade con l'ultimo utente e non con quello schiacciato--->Modificare TradeScene
         tradeList.setEditable(true);
 
         scrollableList.setContent(tradeList);
