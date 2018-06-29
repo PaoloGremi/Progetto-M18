49ce13293008cb7e8b63866b2e7b372b0a58fb5d
MarcoArazzi <marcoarazzi96@gmail.com>
2018-06-29 12:19:20 +0200

    searchUser list

--- src/Interface/SearchUserScene.java
+++ src/Interface/SearchUserScene.java
@@ -3,9 +3,14 @@ package Interface;
 
 import ClientServer.MessageServer;
 import ClientServer.MessageType;
+import TradeCenter.Card.Card;
 import TradeCenter.Customers.Customer;
+import TradeCenter.Trades.Trade;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -44,7 +49,6 @@ public class SearchUserScene {
         pane.setStyle("-fx-background-color: #beff8e;");
 
         search.setOnAction(event -> {
-            int count = 1;
             results.getChildren().removeAll(results.getChildren());
             String searchText = searchString.getText();
             if(searchText == null) searchText = "";     //handling null string
@@ -57,45 +61,27 @@ public class SearchUserScene {
                 ArrayList<Customer> users = (ArrayList<Customer>)(inputStream.readObject());
                 socket.close();
                 if(users != null){
-                    for(Customer customer : users){
-                        TextFlow user = new TextFlow();
-                        HBox hBox = new HBox();
-                        Text username = new Text();
-                        if(count % 2 == 0){
-                            //useraname.setText(customer.getUsername());
-                            username.setText(customer.getUsername());
-                            user.getChildren().add(username);
-                            user.setStyle("-fx-background-color: DAE6A2;");
-                            hBox.getChildren().add(user);
-                            hBox.setStyle("-fx-background-color: DAE6A2;");
-                            count++;
-                        }
-                        else {
-                            username.setText(customer.getUsername());
-                            user.getChildren().add(username);
-                            user.setStyle("-fx-background-color: orange;");
-                            hBox.getChildren().add(user);
-                            hBox.setStyle("-fx-background-color: orange;");
-                            count++;
-                        }
-
-
+                    ObservableList<Customer> customers = FXCollections.observableArrayList();
+                    customers.addAll(users);
+                    JFXListView<Customer> usersList = new JFXListView<>();
+                    usersList.getItems().addAll(users);
+                    if(!customers.isEmpty()){
                         EventHandler<MouseEvent> eventHandlerBox =
                                 new EventHandler<javafx.scene.input.MouseEvent>() {
-
                                     @Override
                                     public void handle(javafx.scene.input.MouseEvent e) {
-                                        MainWindow.refreshDynamicContent(OtherUserProfileScene.display(myProfile, customer));
+                                        Customer otherCustomer = usersList.getSelectionModel().getSelectedItem();
+
+                                            MainWindow.refreshDynamicContent(OtherUserProfileScene.display(mySelf, otherCustomer));
+
                                     }
                                 };
 
-                        user.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, eventHandlerBox);
-                        user.setTextAlignment(TextAlignment.CENTER);
-                        user.setPrefSize(100,10);
-                        //hBox.setFillHeight(true);
-                        hBox.setPadding(new Insets(5,400,5,397));
-                        results.getChildren().add(hBox);
+                        //todo se funziona listener modificare tradeScene (magari metodo refresh)in modo che si apra come l'ultima offerta fatta e non come fosse la prima
+
+                        usersList.setOnMouseClicked(eventHandlerBox);
                     }
+                    usersList.setEditable(true);
                     results.setAlignment(Pos.CENTER);
                     results.setPadding(new Insets(5));
                     results.setFillWidth(true);
@@ -105,8 +91,8 @@ public class SearchUserScene {
                     resultsArea.setFitToWidth(true);
                     resultsArea.setPadding(new Insets(5));
                     resultsArea.setStyle("-fx-background-color: #beff8e;");
-                    resultsArea.setContent(flow);
-                    scene.setCenter(flow);
+                    resultsArea.setContent(usersList);
+                    scene.setCenter(resultsArea);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
